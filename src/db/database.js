dc64b181e0df19c8f06e33905b5033f727b36493
Vijay Prajapati <vijay@arth.tech>
2020-02-29 21:07:33 +0530

    changes

--- src/db/database.js
+++ src/db/database.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 //  before( () => {
     const connection =  mongoose.connect(
-        process.env.DB_CONNECT,
+        process.env.DB_CONNECT_PROD,
         { useCreateIndex: true, useNewUrlParser: true }, 
         (err, success) => {
             if(err) console.log(err)
